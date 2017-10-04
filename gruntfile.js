183888c019ed866b02c9dd6ccae1516437d22211
Omar Alshaker <omar@omaralshaker.com>
2017-10-04 14:45:46 +0200

    Add `rc` prefix in grunt config

--- gruntfile.js
+++ gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
         push: false,
         // pushTo: 'origin',
         globalReplace: false,
-        prereleaseName: false,
+        prereleaseName: 'rc',
         regExp: false
       }
     }
